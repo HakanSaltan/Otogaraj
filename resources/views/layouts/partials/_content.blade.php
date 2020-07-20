af1287a1deb6bcd14d0e902f2053a237bdedc9a5
burkoner <61681147+burkoner@users.noreply.github.com>
2020-07-20 22:48:04 +0300

    uye-islemler

--- resources/views/layouts/partials/_content.blade.php
+++ resources/views/layouts/partials/_content.blade.php
@@ -8,6 +8,51 @@
                     <div class="col-xl-12">
                         @yield('content')
                     </div>
+                    <div class="row">
+                        <div class="col-lg-4">
+                            <!--begin::Card-->
+                            <div class="card card-custom card-stretch">
+                                <div class="card-header">
+                                    <div class="card-title">
+                                        <h3 class="card-label">Yeni Araç Ekle <small>Tamir</small></h3>
+                                    </div>
+                                </div>
+                                <div class="card-body text-center">
+                                   <a href=""> <i class="fa fa-car icon-10x text-primary text-hover-success"></i></a>
+                                </div>
+                            </div>
+                            <!--end::Card-->
+                        </div>
+                        <div class="col-lg-4">
+                            <!--begin::Card-->
+                            <div class="card card-custom card-stretch">
+                                <div class="card-header">
+                                    <div class="card-title">
+                                        <h3 class="card-label">Yeni Fatura Oluştur <small>Tamir</small></h3>
+                                    </div>
+                                </div>
+                                <div class="card-body text-center">
+                                    <a href=""> <i class="fa fa-receipt icon-10x text-primary text-hover-success"></i></a>
+                                </div>
+                            </div>
+                            <!--end::Card-->
+                        </div>
+                        <div class="col-lg-4">
+                            <!--begin::Card-->
+                            <div class="card card-custom card-stretch">
+                                <div class="card-header">
+                                    <div class="card-title">
+                                        <h3 class="card-label">Card Title <small>same height cards</small></h3>
+                                    </div>
+                                </div>
+                                <div class="card-body">
+                                    ...
+                                </div>
+                            </div>
+                            <!--end::Card-->
+                        </div>
+                    </div>
+
                 </div>
             </div>
         </div>
--- resources/views/pages/uye/home.blade.php
+++ resources/views/pages/uye/home.blade.php
@@ -1,19 +1,10 @@
-@extends('layouts.app')
+@extends('layouts.uye')
 @section('css')
 @endsection
 @section('baslik')
 Anasayfa
 @endsection
-@section('subheader')
-<h2 class="text-white font-weight-bold my-2 mr-5">Anasayfa</h2>
-@endsection
-@section('subheaderalt')
-<a href="#" class="opacity-75 hover-opacity-100">
-    <i class="flaticon2-shelter text-white icon-1x"></i>
-</a>
-<span class="label label-dot label-sm bg-white opacity-75 mx-3"></span>
-<a href="" class="text-white text-hover-white opacity-75 hover-opacity-100">Anasayfa</a>
-@endsection
+
 @section('content')
 <div class="card card-custom gutter-b card-stretch">
     <div class="card-header border-0 pt-5">
