ef5ba67665dd1af846cffb2153de4e3086bb0857
burkoner <61681147+burkoner@users.noreply.github.com>
2020-07-24 20:22:19 +0300

    üye işlem sayfası düzenlemesi

--- resources/views/layouts/partials/_content.blade.php
+++ resources/views/layouts/partials/_content.blade.php
@@ -8,52 +8,104 @@
                     <div class="col-xl-12">
                         @yield('content')
                     </div>
-                    <div class="row">
-                        <div class="col-lg-4">
+                        <div class="col-lg-3">
                             <!--begin::Card-->
                             <div class="card card-custom card-stretch">
                                 <div class="card-header">
-                                    <div class="card-title">
-                                        <h3 class="card-label">Yeni Araç Ekle <small>Tamir</small></h3>
+                                    <div class="card-title" style="margin: auto">
+                                        <h3 class="card-label">ARAÇ EKLE</h3>
                                     </div>
                                 </div>
                                 <div class="card-body text-center">
-                                   <a href=""> <i class="fa fa-car icon-10x text-primary text-hover-success"></i></a>
+                                   <a href="" class="text-primary text-hover-success"> <i class="fa fa-car icon-10x text-primary text-hover-success"></i>
+                                   <h2>YENİ ARAÇ EKLE</h2>
+                                   </a>
                                 </div>
                             </div>
                             <!--end::Card-->
                         </div>
-                        <div class="col-lg-4">
+                        <div class="col-lg-3">
                             <!--begin::Card-->
                             <div class="card card-custom card-stretch">
-                                <div class="card-header">
-                                    <div class="card-title">
-                                        <h3 class="card-label">Yeni Fatura Oluştur <small>Tamir</small></h3>
+                                <div class="card-header ">
+                                    <div class="card-title" style="margin: auto">
+                                        <h3 class="card-label">FATURA</h3>
                                     </div>
                                 </div>
                                 <div class="card-body text-center">
-                                    <a href=""> <i class="fa fa-receipt icon-10x text-primary text-hover-success"></i></a>
+                                    <a href="" class="text-primary text-hover-success"> <i class="fa fa-receipt icon-10x text-primary text-hover-success"></i>
+                                    <h2>FATURA OLUŞTUR</h2></a>
                                 </div>
                             </div>
                             <!--end::Card-->
                         </div>
-                        <div class="col-lg-4">
+                        <div class="col-lg-3">
                             <!--begin::Card-->
                             <div class="card card-custom card-stretch">
-                                <div class="card-header">
-                                    <div class="card-title">
-                                        <h3 class="card-label">Card Title <small>same height cards</small></h3>
+                                <div class="card-header ">
+                                    <div class="card-title" style="margin: auto">
+                                        <h3 class="card-label">İŞLEM</h3>
                                     </div>
                                 </div>
-                                <div class="card-body">
-                                    ...
+                                <div class="card-body text-center">
+                                    <a href="" class="text-primary text-hover-success"> <i class="fa fa-tools icon-10x text-primary text-hover-success"></i>
+                                    <h2>İŞLEM OLUŞTUR</h2></a>
                                 </div>
                             </div>
                             <!--end::Card-->
                         </div>
-                    </div>
+
+
+                        <div class="col-lg-3">
+                            <!--begin::Card-->
+                            <div class="card card-custom card-stretch">
+                                <div class="card-header ">
+                                    <div class="card-title" style="margin: auto">
+                                        <h3 class="card-label">MÜŞTERİLERİM</h3>
+                                    </div>
+                                </div>
+                                <div class="card-body text-center">
+                                    <a href="" class="text-primary text-hover-success"> <i class="fa fa-users icon-10x text-primary text-hover-success"></i>
+                                        <h2>KAYITLI MÜŞTERİLERİM</h2></a>
+                                </div>
+                            </div>
+                            <!--end::Card-->
+                        </div>
+
+                        <div class="col-lg-3" style="top: 20px">
+                            <!--begin::Card-->
+                            <div class="card card-custom card-stretch">
+                                <div class="card-header ">
+                                    <div class="card-title" style="margin: auto">
+                                        <h3 class="card-label">FİYAT LİSTESİ</h3>
+                                    </div>
+                                </div>
+                                <div class="card-body text-center">
+                                    <a href="" class="text-primary text-hover-success"> <i class="fa fa-shopping-cart icon-10x text-primary text-hover-success"></i>
+                                        <h2>PARÇA FİYAT LİSTESİ</h2></a>
+                                </div>
+                            </div>
+                            <!--end::Card-->
+                        </div>
+
+                        <div class="col-lg-3" style="top: 20px">
+                            <!--begin::Card-->
+                            <div class="card card-custom card-stretch">
+                                <div class="card-header ">
+                                    <div class="card-title" style="margin: auto">
+                                        <h3 class="card-label">İŞLEM GEÇMİŞİ</h3>
+                                    </div>
+                                </div>
+                                <div class="card-body text-center">
+                                    <a href="" class="text-primary text-hover-success"> <i class="fa fa-history icon-10x text-primary text-hover-success"></i>
+                                    <h2>İŞLEM GEÇMİŞİ</h2></a>
+                                </div>
+                            </div>
+                            <!--end::Card-->
+                        </div>
 
                 </div>
+
             </div>
         </div>
     </div>
