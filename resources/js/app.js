2cceca6592d635f997d8ad9efbed9f24d0dd4e64
batuhanhaymana <32592602+batuhanhaymana@users.noreply.github.com>
2020-09-19 22:55:39 +0300

    merhaba Vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,8 +4,7 @@ require('./bootstrap');
 window.Vue = require('vue');
 
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
-Vue.component('test-component',require('./components/Test').default);
+Vue.component('test',require('./components/Test').default);
 
 const vm = new Vue({
     el: '#app',
